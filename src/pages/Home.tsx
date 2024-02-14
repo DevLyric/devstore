d37a869761fa54b893c0d9af5b57d27973f46dcc
Douglas Souza <lyricsan@hotmail.com>
2024-02-14 17:33:54 -0300

    feat: display products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import { Header } from '../components/Header'
 import { Profile } from '../components/Profile'
 import { ProductFilter } from '../components/ProductFilter'
 import { ShoppingCart } from '../components/ShoppingCart'
+import { ProductGallery } from '../components/ProductGallery'
+import data from '../data.json'
 
 export function Home() {
   return (
@@ -17,6 +19,20 @@ export function Home() {
             <Profile />
           </div>
         </Header>
+
+        <ProductGallery>
+          {data.products.map((product) => (
+            <div key={product.id} className="rounded bg-gray-100">
+              <div className="cursor-pointer transition-all hover:scale-105">
+                <img
+                  className="max-w-80"
+                  src={product.image}
+                  alt="product-image"
+                />
+              </div>
+            </div>
+          ))}
+        </ProductGallery>
       </div>
     </div>
   )
